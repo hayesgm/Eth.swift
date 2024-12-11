99c08845342d33a07e73cb3b914d99c7bbfa3dad
Geoffrey Hayes <hayesgm@gmail.com>
2024-12-11 15:26:11 -0800

    Bump to our own version of Keccak

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/bitflying/SwiftKeccak.git", from: "0.1.0"),
+        .package(url: "https://github.com/hayesgm/SwiftKeccak.git", branch: "98a9d4a037dd62283977d5e0ef7d11c5612ff813"),
         .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
