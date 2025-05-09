dfd06b691f87dc13be28923b9ea6c270511d6da4
Mykel Pereira <me@mykelpereira.com>
2025-05-09 10:37:23 -0700

    Add many more chains (#45)

--- Sources/Eth/Network.swift
+++ Sources/Eth/Network.swift
@@ -3,21 +3,38 @@ import Foundation
 
 /// A ``Network`` represents different Ethereum networks (e.g. `Mainnet` or `Base`).
 public enum Network: Codable, Equatable, Hashable, Identifiable {
+    case alephZero
     case arbitrum
     case arbitrumSepolia
     case avalanche
     case base
     case baseSepolia
+    case blast
+    case bnbSmartChain
+    case celo
     case ethereum
+    case gnosis
+    case hyperEVM
+    case ink
+    case lens
     case linea
     case lineaSepolia
+    case lisk
+    case mantle
+    case mode
     case optimism
+    case plume
     case polygon
+    case redstone
     case scroll
     case scrollSepolia
     case sepolia
-    case worldChain
+    case soneium
+    case sonic
     case unichain
+    case worldChain
+    case zkSync
+    case zora
     case unknown(BigInt)
 
     public var id: String {
@@ -26,6 +43,8 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
 
     public var description: String {
         switch self {
+        case .alephZero:
+            "Aleph Zero"
         case .arbitrum:
             "Arbitrum"
         case .arbitrumSepolia:
@@ -36,26 +55,58 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
             "Base"
         case .baseSepolia:
             "Base (Sepolia)"
+        case .blast:
+            "Blast"
+        case .bnbSmartChain:
+            "BNB Smart Chain"
+        case .celo:
+            "Celo"
         case .ethereum:
             "Ethereum"
+        case .gnosis:
+            "Gnosis"
+        case .hyperEVM:
+            "HyperEVM"
+        case .ink:
+            "Ink"
+        case .lens:
+            "Lens"
         case .linea:
             "Linea"
         case .lineaSepolia:
             "Linea (Sepolia)"
+        case .lisk:
+            "Lisk"
+        case .mantle:
+            "Mantle"
+        case .mode:
+            "Mode"
         case .optimism:
             "Optimism"
+        case .plume:
+            "Plume"
         case .polygon:
             "Polygon"
+        case .redstone:
+            "Redstone"
         case .scroll:
             "Scroll"
         case .scrollSepolia:
             "Scroll (Sepolia)"
         case .sepolia:
             "Sepolia"
-        case .worldChain:
-            "World Chain"
+        case .soneium:
+            "Soneium"
+        case .sonic:
+            "Sonic"
         case .unichain:
             "Unichain"
+        case .worldChain:
+            "World Chain"
+        case .zkSync:
+            "ZkSync"
+        case .zora:
+            "Zora"
         case let .unknown(chainId):
             "Unknown (chainId=\(chainId.description))"
         }
@@ -93,6 +144,8 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
     /// if the chainId  is not supported
     public static func fromChainId(_ chainId: BigInt) -> Self {
         switch chainId {
+        case Network.alephZero.chainId:
+            .alephZero
         case Network.arbitrum.chainId:
             .arbitrum
         case Network.arbitrumSepolia.chainId:
@@ -103,26 +156,58 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
             .base
         case Network.baseSepolia.chainId:
             .baseSepolia
+        case Network.blast.chainId:
+            .blast
+        case Network.bnbSmartChain.chainId:
+            .bnbSmartChain
+        case Network.celo.chainId:
+            .celo
         case Network.ethereum.chainId:
             .ethereum
+        case Network.gnosis.chainId:
+            .gnosis
+        case Network.hyperEVM.chainId:
+            .hyperEVM
+        case Network.ink.chainId:
+            .ink
+        case Network.lens.chainId:
+            .lens
         case Network.linea.chainId:
             .linea
         case Network.lineaSepolia.chainId:
-            .lineaSepolia
+            .lineaSepolia   
+        case Network.lisk.chainId:
+            .lisk
+        case Network.mantle.chainId:
+            .mantle
+        case Network.mode.chainId:
+            .mode
         case Network.optimism.chainId:
             .optimism
+        case Network.plume.chainId:
+            .plume
         case Network.polygon.chainId:
             .polygon
+        case Network.redstone.chainId:
+            .redstone
         case Network.scroll.chainId:
             .scroll
         case Network.scrollSepolia.chainId:
             .scrollSepolia
         case Network.sepolia.chainId:
             .sepolia
-        case Network.worldChain.chainId:
-            .worldChain
+        case Network.soneium.chainId:
+            .soneium
+        case Network.sonic.chainId:
+            .sonic
         case Network.unichain.chainId:
             .unichain
+        case Network.worldChain.chainId:
+            .worldChain
+        case Network.zkSync.chainId:
+            .zkSync
+        case Network.zora.chainId:
+            .zora
         default:
             .unknown(chainId)
         }
@@ -130,6 +215,8 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
 
     public var chainId: BigInt {
         switch self {
+        case .alephZero:
+            BigInt(41455)
         case .arbitrum:
             BigInt(42161)
         case .arbitrumSepolia:
@@ -140,29 +227,61 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
             BigInt(8453)
         case .baseSepolia:
             BigInt(84532)
+        case .blast:
+            BigInt(81457)
+        case .bnbSmartChain:
+            BigInt(56)
+        case .celo:
+            BigInt(42220)
         case .ethereum:
             BigInt(1)
+        case .gnosis:
+            BigInt(100)
+        case .hyperEVM:
+            BigInt(999)
+        case .ink:
+            BigInt(57073)
+        case .lens:
+            BigInt(232)
         case .linea:
             BigInt(59144)
         case .lineaSepolia:
             BigInt(59141)
+        case .lisk:
+            BigInt(1135)
+        case .mantle:
+            BigInt(5000)
+        case .mode:
+            BigInt(34443)
+        case .plume:
+            BigInt(98865)
         case .optimism:
             BigInt(10)
         case .polygon:
             BigInt(137)
+        case .redstone:
+            BigInt(690)
         case .scroll:
             BigInt(534_352)
         case .scrollSepolia:
             BigInt(534_351)
+        case .soneium:
+            BigInt(1868)
+        case .sonic:
+            BigInt(146)
         case .sepolia:
             BigInt(11_155_111)
-        case .worldChain:
-            BigInt(480)
         case .unichain:
             BigInt(130)
+        case .worldChain:
+            BigInt(480)
+        case .zkSync:
+            BigInt(324)
+        case .zora:
+            BigInt(7777777)
         case .unknown:
             BigInt(0)
-        }
+}
     }
 
     public var explorerName: String? {
@@ -199,11 +318,17 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
             "Uniscan"
         case .unknown:
             nil
+        case .alephZero, .blast, .bnbSmartChain, .celo, .gnosis, .hyperEVM, 
+             .ink, .lens, .lisk, .mantle, .mode, .plume, .redstone, 
+             .soneium, .sonic, .zkSync, .zora:
+            "\(self.description) Explorer"
         }
     }
 
     public var explorerUrl: URL? {
         switch self {
+        case .alephZero:
+            URL(string: "https://evm-explorer.alephzero.org")
         case .arbitrum:
             URL(string: "https://arbiscan.io")
         case .arbitrumSepolia:
@@ -214,26 +339,58 @@ public enum Network: Codable, Equatable, Hashable, Identifiable {
             URL(string: "https://basescan.org")
         case .baseSepolia:
             URL(string: "https://sepolia.basescan.org")
+        case .blast:
+            URL(string: "https://blastscan.io")
+        case .bnbSmartChain:
+            URL(string: "https://bscscan.com")
+        case .celo:
+            URL(string: "https://celoscan.io")
         case .ethereum:
             URL(string: "https://etherscan.io")
+        case .gnosis:
+            URL(string: "https://gnosisscan.io")
+        case .hyperEVM:
+            URL(string: "https://app.hyperliquid.xyz/explorer")
+        case .ink:
+            URL(string: "https://explorer.inkonchain.com")
+        case .lens:
+            URL(string: "https://explorer.lens.xyz")
         case .linea:
             URL(string: "https://lineascan.build")
         case .lineaSepolia:
             URL(string: "https://sepolia.lineascan.build")
+        case .lisk:
+            URL(string: "https://blockscout.lisk.com")
+        case .mantle:
+            URL(string: "https://mantlescan.xyz")
+        case .mode:
+            URL(string: "https://explorer.mode.network")
+        case .plume:
+            URL(string: "https://explorer.plumenetwork.xyz")
         case .optimism:
             URL(string: "https://optimistic.etherscan.io")
         case .polygon:
             URL(string: "https://polygonscan.com")
+        case .redstone:
+            URL(string: "https://explorer.redstone.xyz")
         case .scroll:
             URL(string: "https://scrollscan.com")
         case .scrollSepolia:
             URL(string: "https://sepolia.scrollscan.com")
         case .sepolia:
             URL(string: "https://sepolia.etherscan.io")
-        case .worldChain:
-            URL(string: "https://worldscan.org/")
+        case .soneium:
+            URL(string: "https://soneium.blockscout.com")
+        case .sonic:
+            URL(string: "https://sonicscan.org")
         case .unichain:
             URL(string: "https://uniscan.xyz")
+        case .worldChain:
+            URL(string: "https://worldscan.org/")
+        case .zkSync:
+            URL(string: "https://zksync.blockscout.com")
+        case .zora:
+            URL(string: "https://explorer.zora.energy")
         case .unknown:
             nil
         }
